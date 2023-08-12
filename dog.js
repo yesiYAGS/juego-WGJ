ead6a373d3529ccfa22e0fe79c727da9df88ffb7
Francisca Medina Concha <fbmedina@uc.cl>
2023-08-12 14:15:08 -0400

    added limits with dogtrail and the end of the grid

--- dog.js
+++ dog.js
@@ -1,38 +1,25 @@
 let posX = 0;
 let posY = 0;
 let size = 40;
+let dogTrail = [];
 let totalOfSquares = 5;
+let redSquares = [];
 
 function setup() {
   createCanvas(size * 10, size * 10);
   background(200);
+  drawGrid();
+  drawRandomRedSquares(totalOfSquares);
+}
+
+function drawGrid() {
   for (var x = 0; x < width; x += size) {
-		for (var y = 0; y < height; y += size) {
-			stroke(0);
-			strokeWeight(1);
-			line(x, 0, x, height);
-			line(0, y, width, y);
-		}
-	}
-  drawRandomRedSquares(totalOfSquares);  
-}
-
-function draw() {
-  fill(0);
-  rect(posX, posY, size, size);
-}
-
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    posX = posX - size;
-  } else if (keyCode === RIGHT_ARROW) {
-    posX = posX + size;
-  }
-
-  if (keyCode === DOWN_ARROW) {
-    posY = posY + size;
-  } else if (keyCode === UP_ARROW) {
-    posY = posY - size;
+    for (var y = 0; y < height; y += size) {
+      stroke(0);
+      strokeWeight(1);
+      line(x, 0, x, height);
+      line(0, y, width, y);
+    }
   }
 }
 
@@ -41,8 +28,51 @@ function drawRandomRedSquares(count) {
     let gridX = int(random(width / size));
     let gridY = int(random(height / size));
     let x = gridX * size + (size - size / 2) / 2;
-    let y = gridY * size + (size -  size / 2) / 2;
+    let y = gridY * size + (size - size / 2) / 2;
+    redSquares.push({ x: x, y: y });
     fill(255, 0, 0);
     rect(x, y, size / 2, size / 2);
+  }
+}
+
+function draw() {
+  background(200);
+  drawGrid();
+  for (let { x, y } of redSquares) {
+    fill(255, 0, 0);
+    rect(x, y, 20, 20);
+  }
+  fill(0);
+  for (let { x, y } of dogTrail) {
+    rect(x, y, size, size);
+  }
+  rect(posX, posY, size, size);
+}
+
+
+function keyPressed() {
+  let newX = posX;
+  let newY = posY;
+
+  if (keyCode === LEFT_ARROW) {
+    newX -= size;
+  } else if (keyCode === RIGHT_ARROW) {
+    newX += size;
+  } else if (keyCode === DOWN_ARROW) {
+    newY += size;
+  } else if (keyCode === UP_ARROW) {
+    newY -= size;
+  }
+
+  if (
+    newX >= 0 &&
+    newY >= 0 &&
+    newX < width &&
+    newY < height &&
+    !dogTrail.some(sq => sq.x === newX && sq.y === newY)
+  ) {
+    dogTrail.push({ x: posX, y: posY });
+    posX = newX;
+    posY = newY;
   }
 }
